cdddeb64c3b2f327dfc1be48facbaf06656e8efe
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2020-11-07 19:30:32 -0300

    Fixed routing issue

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const routes = require('./routes');
 
 // Request JSON treatment middleware
 router.use(express.json());
 
-// Default index route
-router.get('/', (req, res, next) => {
-    res.status(200).send({ status: "OK" });
-});
-
 // Import routes
+router.use(routes);
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,2 +1,8 @@
 const router = require('express').Router();
 
+// Default index route
+router.get('/', (req, res, next) => {
+    res.status(200).send({ status: "OK" });
+});
+
+module.exports = router;
